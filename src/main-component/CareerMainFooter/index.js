050564fefefaed618ed0c8fe1672b817fc5da368
Monica <monica.b.raju001@gmail.com>
2024-11-22 09:40:48 +0530

    updated career page image

--- src/main-component/CareerMainFooter/index.js
+++ src/main-component/CareerMainFooter/index.js
@@ -5,7 +5,7 @@ import Header from "../../components/header";
 import Footer from "../../components/footer";
 import CareerFooter from "../../components/CareerFooter/careerFooter";
 import PageTitle from "../../components/pagetitle";
-import bg from "../../components/assets/careersedited.jpg";
+import bg from "../../components/assets/career_page.jpg";
 
 const CareerMainFooterComp = () => {
   return (
